f6b8983eafa077bf36d50fe114a178109ab95847
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-07-17 12:02:48 +0200

    Create Project

---
+++ src/main/java/lucadipietro/ClimbHill_BackEnd/ClimbHillBackEndApplication.java
@@ -0,0 +1,13 @@
+package lucadipietro.ClimbHill_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbHillBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbHillBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lucadipietro/ClimbHill_BackEnd/ClimbHillBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lucadipietro.ClimbHill_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbHillBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
