cf2c4390507769e26b834962137b5a94fa9f0e8e
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-07-27 02:31:26 +0200

    Add getTipoTorneo

--- src/main/java/lucadipietro/ClimbHill_BackEnd/enums/TipoTorneo.java
+++ src/main/java/lucadipietro/ClimbHill_BackEnd/enums/TipoTorneo.java
@@ -1,5 +1,17 @@
 package lucadipietro.ClimbHill_BackEnd.enums;
 
+import lucadipietro.ClimbHill_BackEnd.exceptions.BadRequestException;
+
 public enum TipoTorneo {
-    TORNEO_SINGOLO,TORNEO_A_SQUADRE
+    TORNEO_SINGOLO,TORNEO_A_SQUADRE;
+
+    public static TipoTorneo getTipoTorneo(String tipoTorneo) {
+        if (tipoTorneo.equalsIgnoreCase("torneo_singolo")) {
+            return TipoTorneo.TORNEO_SINGOLO;
+        } else if (tipoTorneo.equalsIgnoreCase("torneo_a_squadre")) {
+            return TipoTorneo.TORNEO_A_SQUADRE;
+        } else {
+            throw new BadRequestException("I Tipi di torneo possibili sono: TORNEO_SINGOLO O TORNEO_A_SQUADRE");
+        }
+    }
 }
