7bff73f190fdee141cc65beecdd6849f81c8bfd3
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-07-27 18:51:14 +0200

    Finish giochi service

--- src/main/java/lucadipietro/ClimbHill_BackEnd/repositories/GiochiRepository.java
+++ src/main/java/lucadipietro/ClimbHill_BackEnd/repositories/GiochiRepository.java
@@ -4,8 +4,10 @@ import lucadipietro.ClimbHill_BackEnd.entities.Gioco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface GiochiRepository extends JpaRepository<Gioco, UUID> {
+    Optional<Gioco> findByNome(String nome);
 }
--- src/main/java/lucadipietro/ClimbHill_BackEnd/services/GiochiService.java
+++ src/main/java/lucadipietro/ClimbHill_BackEnd/services/GiochiService.java
@@ -1,7 +1,54 @@
 package lucadipietro.ClimbHill_BackEnd.services;
 
+import lucadipietro.ClimbHill_BackEnd.entities.Gioco;
+import lucadipietro.ClimbHill_BackEnd.entities.Utente;
+import lucadipietro.ClimbHill_BackEnd.exceptions.BadRequestException;
+import lucadipietro.ClimbHill_BackEnd.exceptions.NotFoundException;
+import lucadipietro.ClimbHill_BackEnd.payloads.GiocoDTO;
+import lucadipietro.ClimbHill_BackEnd.repositories.GiochiRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class GiochiService {
+
+    @Autowired
+    private GiochiRepository giochiRepository;
+
+    public Gioco save(GiocoDTO body){
+        giochiRepository.findByNome(body.nome()).ifPresent(
+                gioco -> {
+                    throw new BadRequestException("Il gioco " + body.nome() + " è gia presente!");
+                });
+        Gioco nuovoGioco = new Gioco(body.nome(), body.descrizione());
+        return this.giochiRepository.save(nuovoGioco);
+    }
+
+    public Gioco findById(UUID giocoId) {
+        return this.giochiRepository.findById(giocoId).orElseThrow(() -> new NotFoundException(giocoId));
+    }
+
+    public Page<Gioco> getGiochi(int pageNumber, int pageSize, String sortBy) {
+        if (pageSize > 50) pageSize = 50;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(sortBy));
+        return giochiRepository.findAll(pageable);
+    }
+
+    public Gioco findByIdAndUpdate(UUID giocoId, GiocoDTO body) {
+        Gioco found = this.findById(giocoId);
+        found.setNome(body.nome());
+        found.setDescrizione(body.descrizione());
+        return this.giochiRepository.save(found);
+    }
+
+    public void findByIdAndDelete(UUID giocoId) {
+        Gioco found = this.findById(giocoId);
+        this.giochiRepository.delete(found);
+    }
 }
